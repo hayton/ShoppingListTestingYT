2f3e1e866b923fbd5f4f3fe7b9b7ead90d7d2a54
hayton <hayton.leung@101medialab.com>
2023-09-06 01:46:58 +0800

    implement navigation and add test case for navigation

--- app/src/main/java/com/androiddevs/shoppinglisttestingyt/ui/AddShoppingItemFragment.kt
+++ app/src/main/java/com/androiddevs/shoppinglisttestingyt/ui/AddShoppingItemFragment.kt
@@ -1,11 +1,49 @@
 package com.androiddevs.shoppinglisttestingyt.ui
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.androiddevs.shoppinglisttestingyt.R
+import com.androiddevs.shoppinglisttestingyt.databinding.FragmentAddShoppingItemBinding
 
 class AddShoppingItemFragment: Fragment(R.layout.fragment_add_shopping_item) {
 
+    private lateinit var binding: FragmentAddShoppingItemBinding
+
     val viewModel by activityViewModels<ShoppingViewModel>()
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentAddShoppingItemBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        //todo: add test case to test navigation
+        binding.ivShoppingImage.setOnClickListener {
+            findNavController().navigate(
+                AddShoppingItemFragmentDirections.actionAddShoppingItemFragmentToImagePickFragment()
+            )
+        }
+
+        val callback = object: OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                //todo: test case to test url is empty
+                viewModel.setCurImageUrl("")
+                findNavController().popBackStack()
+            }
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(callback)
+    }
 }
--- app/src/main/java/com/androiddevs/shoppinglisttestingyt/ui/ShoppingFragment.kt
+++ app/src/main/java/com/androiddevs/shoppinglisttestingyt/ui/ShoppingFragment.kt
@@ -1,12 +1,16 @@
 package com.androiddevs.shoppinglisttestingyt.ui
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.androiddevs.shoppinglisttestingyt.R
+import com.androiddevs.shoppinglisttestingyt.databinding.FragmentShoppingBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -14,8 +18,24 @@ class ShoppingFragment: Fragment(R.layout.fragment_shopping) {
 
     val viewModel by activityViewModels<ShoppingViewModel>()
 
+    private lateinit var binding: FragmentShoppingBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = FragmentShoppingBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.fabAddShoppingItem.setOnClickListener {
+            findNavController().navigate(
+                ShoppingFragmentDirections.actionShoppingFragmentToAddShoppingItemFragment()
+            )
+        }
     }
 }
